203dc828df5412ce4411c54719b27aa6427ac876
CoderYoo <1084063695@qq.com>
2024-11-01 19:02:03 +0800

    style: delelte semicolon

--- src/utils/gamepad.ts
+++ src/utils/gamepad.ts
@@ -1,33 +1,34 @@
 /** 校验是否存在摇杆 */
 export const checkGamepad = () => {
-  const gamepads = navigator.getGamepads();
+  navigator.getGamepads()
+  const gamepads = navigator.getGamepads()
   for (const gamepad of gamepads) {
     if (gamepad) {
-      return true;
+      return true
     }
   }
-  return false;
-};
+  return false
+}
 
 /** 校验油门是否处于中位 */
 export const checkUavIsZero = () => {
-  const gamepads = navigator.getGamepads();
+  const gamepads = navigator.getGamepads()
   for (const gamepad of gamepads) {
     if (!gamepad) {
-      continue;
+      continue
     }
-    const match = gamepad.id.match(/\(([^)]+)\)/);
+    const match = gamepad.id.match(/\(([^)]+)\)/)
     if (!match) {
-      continue;
+      continue
     }
-    const padId = match[1];
+    const padId = match[1]
     if (padId === 'Vendor: 0738 Product: a221') {
-      const z = gamepad.axes[0] - 0.35;
+      const z = gamepad.axes[0] - 0.35
       if (Math.abs(z) < 0.1) {
-        return true;
+        return true
       }
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
