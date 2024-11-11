a29065fd70263485e89e226645aade4fcf7b421d
CoderYoo <1084063695@qq.com>
2024-11-11 16:40:10 +0800

    chore: legacy config

--- vite-config/common.ts
+++ vite-config/common.ts
@@ -3,10 +3,18 @@ import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
 import AutoImport from '../plugins/auto-import'
 import path from 'path'
+import legacy from '@vitejs/plugin-legacy'
 
 // 公共的配置, 开发时, 生产时都需要的配置
 export default defineConfig({
-  plugins: [react(), cesium(), AutoImport],
+  plugins: [
+    react(),
+    cesium(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    AutoImport,
+  ],
   // root 默认是 process.cwd()，即执行 vite 命令的当前目录
   resolve: {
     alias: {
