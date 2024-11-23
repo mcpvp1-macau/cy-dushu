3b23e54a970520afb880ba61305a4907353e41ca
CoderYoo <1084063695@qq.com>
2024-11-23 15:10:30 +0800

    fix: issue of sending value of ws state

--- src/pages/control-room/uav/hooks/useZoomFactorChange.ts
+++ src/pages/control-room/uav/hooks/useZoomFactorChange.ts
@@ -1,47 +1,39 @@
 import { useDeviceDetailStore } from '@/pages/right/DeviceDetail/hooks/useDeviceDetail.store'
 import { useUavControlRoomStore } from '@/store/context-store/useUavControlRoom.store'
 import { limitNum } from '@/utils/math'
-import { useDebounceEffect, useDebounceFn } from 'ahooks'
+import { useDebounceEffect, useDebounceFn, useLatest } from 'ahooks'
 import { isNil } from 'lodash'
 
 /** 用于控制变焦的 hooks 需要保证有 uavControlRoomStore 和 deviceDetail 的上下文 */
 const useUavZoomFactorChange = () => {
   const zoomFactor = useUavControlRoomStore((s) => s.state.zoomFactor)
   const [value, _setValue] = useState(zoomFactor ?? 2)
+  const valueLatest = useLatest(value)
 
-  const sync = useRef(true)
-  const { run: allowSync } = useDebounceFn(
+  // 同步来自 ws 的焦距值
+  useDebounceEffect(
     () => {
-      sync.current = true
       if (!isNil(zoomFactor) && zoomFactor !== value) {
         _setValue(zoomFactor)
       }
     },
-    { wait: 4000 },
+    [zoomFactor],
+    { wait: 1024 },
   )
-
-  useEffect(() => {
-    if (!isNil(zoomFactor) && sync.current) {
-      _setValue(zoomFactor)
-    }
-  }, [zoomFactor])
 
   /** 设置焦距值, 在设置之后 4s 内不会被受 ws 影响 */
   const setValue = useMemoizedFn<React.Dispatch<React.SetStateAction<number>>>(
     (v: number | ((v: number) => number)) => {
-      sync.current = false
       if (typeof v === 'function') {
         _setValue((old) => limitNum(v(old), 2, 200))
       } else {
         _setValue(limitNum(v, 2, 200))
       }
-      allowSync()
+      sendZoomFactorValue()
     },
   )
 
-  const videoId = useDeviceDetailStore(
-    (s) => s.deviceDetail?.properties?.videoList?.[0]?.videoId,
-  )
+  // 变焦功能物模型
   const deviceModel = useDeviceDetailStore((s) => s.deviceDetail?.deviceModel)
   const zoomfactor = useMemo(
     () =>
@@ -50,18 +42,18 @@ const useUavZoomFactorChange = () => {
       ),
     [deviceModel],
   )
+
   const sendCommand = useUavControlRoomStore((s) => s.sendCommand)
-  useDebounceEffect(
+  const videoId = useDeviceDetailStore(
+    (s) => s.deviceDetail?.properties?.videoList?.[0]?.videoId,
+  )
+  const { run: sendZoomFactorValue } = useDebounceFn(
     () => {
-      if (!zoomfactor) {
-        return
-      }
       sendCommand('service.liveZoomChange.post', {
         videoId,
-        [zoomfactor.identifier]: value,
+        [zoomfactor.identifier]: valueLatest.current,
       })
     },
-    [value],
     { wait: 500 },
   )
 
