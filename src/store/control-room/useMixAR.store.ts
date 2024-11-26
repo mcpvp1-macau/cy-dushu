fdb657b26584c811fb2ce6fc76018fbd87dd11a5
CoderYoo <1084063695@qq.com>
2024-11-26 19:12:47 +0800

    style: delete semicolon

--- src/store/control-room/useMixAR.store.ts
+++ src/store/control-room/useMixAR.store.ts
@@ -1,69 +1,84 @@
-import { Feature, GeoJsonProperties, Geometry } from 'geojson';
-import { RBush } from 'geojson-rbush';
-import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { Feature, GeoJsonProperties, Geometry } from 'geojson'
+import { RBush } from 'geojson-rbush'
+import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
 /** UAV 属性 */
 export type UavProperties = Partial<{
-  altitude: number;
-  cameraType: number;
-  gimbalYaw: number;
-  gimbalPitch: number;
-  height: number;
-  latitude: number;
-  lensType: number;
-  longitude: number;
-  width: number;
-  zoomFactor: number;
-}>;
+  altitude: number
+  cameraType: number
+  gimbalYaw: number
+  gimbalPitch: number
+  height: number
+  latitude: number
+  lensType: number
+  longitude: number
+  width: number
+  zoomFactor: number
+}>
 
 /** Cesium 在地图上的四个角 */
 export type GimbalPick = Partial<{
-  leftBottom?: number[];
-  leftTop?: number[];
-  rightTop?: number[];
-  rightBottom?: number[];
-  center?: number[];
-}>;
+  leftBottom?: number[]
+  leftTop?: number[]
+  rightTop?: number[]
+  rightBottom?: number[]
+  center?: number[]
+}>
 
 /** AR */
 export type ArFeature = {
-  coodinates: number[][];
-  properties?: any;
-  isClosed?: boolean;
-};
+  coodinates: number[][]
+  properties?: any
+  isClosed?: boolean
+}
 
 /** 起飞时的信息 */
 export type StartInfo = {
-  startAGL: number;
-  startHeight: number;
-};
+  startAGL: number
+  startHeight: number
+}
 
 type StateType = {
-  enable: boolean;
-  uavProperties: UavProperties;
-  referenceLastAR: boolean;
-  referenceLastMap: boolean;
-  fov: number[][];
-  features: Feature[];
-  arData: ArFeature[];
-  startInfo: StartInfo;
-  source_frame_height: number;
-  source_frame_width: number;
-  gimbalPick: GimbalPick;
-  rTree: RBush<Geometry, GeoJsonProperties> | null;
-};
+  enable: boolean
+  uavProperties: UavProperties
+  referenceLastAR: boolean
+  referenceLastMap: boolean
+  fov: number[][]
+  features: Feature[]
+  arData: ArFeature[]
+  startInfo: StartInfo
+  source_frame_height: number
+  source_frame_width: number
+  gimbalPick: GimbalPick
+  rTree: RBush<Geometry, GeoJsonProperties> | null
+  /** 航线信息 */
+  airpointPositions: { pointX: number; pointY: number; pointZ: number }[]
+  /** 航线 AR */
+  airpointPositionsAR: number[][]
+  /** 覆盖物 AR */
+  overlaiesAR: number[][][]
+}
 
 type ActionsType = {
-  updateEnable: (enable: boolean) => void;
-  updateFeatures: (features: StateType['features']) => void;
-  updateState: (state: Partial<StateType>) => void;
-  updateUavProperties: (uavProperties: UavProperties) => void;
-  updateArData: (arData: StateType['arData']) => void;
-  updateStartInfo: (startInfo: StateType['startInfo']) => void;
-  updateGimbalPick: (gimbalPick: StateType['gimbalPick']) => void;
-  updateRTree: (rTree: StateType['rTree']) => void;
-};
+  updateEnable: (enable: boolean) => void
+  updateFeatures: (features: StateType['features']) => void
+  updateState: (state: Partial<StateType>) => void
+  updateUavProperties: (uavProperties: UavProperties) => void
+  updateArData: (arData: StateType['arData']) => void
+  updateStartInfo: (startInfo: StateType['startInfo']) => void
+  updateGimbalPick: (gimbalPick: StateType['gimbalPick']) => void
+  updateRTree: (rTree: StateType['rTree']) => void
+  /** 更新航线信息 */
+  updateAirpointPositions: (
+    airpointPositions: StateType['airpointPositions'],
+  ) => void
+  /** 更新航线 AR */
+  updateAirpointPositionsAR: (
+    airpointPositionsAR: StateType['airpointPositionsAR'],
+  ) => void
+  updateOverlaiesAR: (overlaiesAR: StateType['overlaiesAR']) => void
+}
 
 /** 虚实融合 */
 const useMixARStore = create<StateType & ActionsType>()(
@@ -84,17 +99,20 @@ const useMixARStore = create<StateType & ActionsType>()(
         startHeight: 0,
       },
       rTree: null,
+      airpointPositions: [],
+      airpointPositionsAR: [],
+      overlaiesAR: [],
       updateEnable: (enable) => {
-        set({ enable, arData: [], features: [] }, false, 'updateEnable');
+        set({ enable, arData: [], features: [] }, false, 'updateEnable')
       },
       updateFeatures: (features) => {
-        set({ features }, false, 'updateFeatures');
+        set({ features }, false, 'updateFeatures')
       },
       updateStartInfo: (startInfo) => {
-        set({ startInfo }, false, 'updateStartInfo');
+        set({ startInfo }, false, 'updateStartInfo')
       },
       updateState: (state) => {
-        set((prev) => ({ ...prev, ...state }), false, 'updateState');
+        set((prev) => ({ ...prev, ...state }), false, 'updateState')
       },
       updateUavProperties: (uavProperties) => {
         set(
@@ -105,16 +123,25 @@ const useMixARStore = create<StateType & ActionsType>()(
           },
           false,
           'updateUavProperties',
-        );
+        )
       },
       updateGimbalPick: (gimbalPick) => {
-        set({ gimbalPick }, false, 'updateGimbalPick');
+        set({ gimbalPick }, false, 'updateGimbalPick')
       },
       updateArData: (arData) => {
-        set({ arData }, false, 'updateArData');
+        set({ arData }, false, 'updateArData')
       },
       updateRTree: (rTree) => {
-        set({ rTree }, false, 'updateRTree');
+        set({ rTree }, false, 'updateRTree')
+      },
+      updateAirpointPositions: (airpointPositions) => {
+        set({ airpointPositions }, false, 'updateAirpointPositions')
+      },
+      updateAirpointPositionsAR: (airpointPositionsAR) => {
+        set({ airpointPositionsAR }, false, 'updateAirpointPositionsAR')
+      },
+      updateOverlaiesAR: (overlaiesAR) => {
+        set({ overlaiesAR }, false, 'updateOverlaiesAR')
       },
     }),
     {
@@ -122,6 +149,6 @@ const useMixARStore = create<StateType & ActionsType>()(
       enabled: process.env.NODE_ENV === 'development' && false,
     },
   ),
-);
+)
 
-export default useMixARStore;
+export default useMixARStore
