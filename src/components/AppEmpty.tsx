af596c540dbdc7df84a1be3a3e11e03a1b0be8b3
CoderYoo <1084063695@qq.com>
2024-12-02 17:17:33 +0800

    feat: change empty image

--- src/components/AppEmpty.tsx
+++ src/components/AppEmpty.tsx
@@ -6,14 +6,14 @@ type PropsType = GetProps<typeof Empty>
 
 const AppEmpty: FC<PropsType> = ({ ...restProps }) => {
   restProps.description ??= '暂无数据'
-  restProps.imageStyle ??= { height: 42 }
+  restProps.imageStyle ??= { height: 32 }
 
   return (
     <Empty
       image={zanwushujuSvg}
       {...restProps}
       className={clsx(
-        'flex flex-col items-center justify-center my-3 opacity-50',
+        'flex flex-col items-center justify-center my-3 opacity-50 text-xs',
         restProps.className,
       )}
     />
