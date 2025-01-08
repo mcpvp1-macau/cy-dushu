18586fba13a44d7acb49e3c853f1db320c792322
CoderYoo <1084063695@qq.com>
2025-01-08 15:40:55 +0800

    feat: add avoid distance display

--- src/pages/control-room/uav/components/ControlRoomVideo/components/Avoidance.tsx
+++ src/pages/control-room/uav/components/ControlRoomVideo/components/Avoidance.tsx
@@ -24,31 +24,68 @@ const Avoidance: FC<PropsType> = memo(() => {
   }
 
   return (
-    <div className="absolute inset-0 pointer-events-none select-none opacity-50">
+    <div className="absolute inset-0 pointer-events-none select-none">
+      <div className="absolute inset-0 opacity-50">
+        <div
+          className={clsx(
+            baseCls,
+            'inset-x-0 top-0 h-7 bg-gradient-to-b from-red-500 to-transparent',
+          )}
+        ></div>
+        <div
+          className={clsx(
+            baseCls,
+            'inset-x-0 bottom-0 h-7 bg-gradient-to-t from-red-500 to-transparent',
+          )}
+        ></div>
+        <div
+          className={clsx(
+            baseCls,
+            'inset-y-0 left-0 w-7 bg-gradient-to-r from-red-500 to-transparent',
+          )}
+        ></div>
+        <div
+          className={clsx(
+            baseCls,
+            'inset-y-0 right-0 w-7 bg-gradient-to-l from-red-500 to-transparent',
+          )}
+        ></div>
+      </div>
       <div
-        className={clsx(
-          baseCls,
-          'inset-x-0 top-0 h-7 bg-gradient-to-b from-red-500 to-transparent',
+        className="absolute inset-0 text-fore bg-opacity-70 text-sm"
+        style={{ textShadow: '0 0 1px red' }}
+      >
+        {(avoidDistances[0] ?? 0x3f) < 15 && (
+          <div className="absolute top-0 left-1/2 -translate-x-1/2">
+            {avoidDistances[0]!.toFixed(1)} m
+          </div>
         )}
-      ></div>
-      <div
-        className={clsx(
-          baseCls,
-          'inset-x-0 bottom-0 h-7 bg-gradient-to-t from-red-500 to-transparent',
+        {(avoidDistances[1] ?? 0x3f) < 15 && (
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+            {avoidDistances[1]!.toFixed(1)} m
+          </div>
         )}
-      ></div>
-      <div
-        className={clsx(
-          baseCls,
-          'inset-y-0 left-0 w-7 bg-gradient-to-r from-red-500 to-transparent',
+        {(avoidDistances[2] ?? 0x3f) < 15 && (
+          <div
+            className="absolute left-0 top-1/2 -translate-y-1/2"
+            style={{
+              writingMode: 'vertical-rl',
+            }}
+          >
+            {avoidDistances[2]!.toFixed(1)} m
+          </div>
         )}
-      ></div>
-      <div
-        className={clsx(
-          baseCls,
-          'inset-y-0 right-0 w-7 bg-gradient-to-l from-red-500 to-transparent',
+        {(avoidDistances[3] ?? 0x3f) < 15 && (
+          <div
+            className="absolute right-0 top-1/2 -translate-y-1/2"
+            style={{
+              writingMode: 'vertical-rl',
+            }}
+          >
+            {avoidDistances[3]!.toFixed(1)} m
+          </div>
         )}
-      ></div>
+      </div>
     </div>
   )
 })
