8c9e80e22206f81a6bb5db39f90a6873c8737f39
17758000300 <1056449783@qq.com>
2025-01-12 15:10:03 +0800

    feat: 更新播放器

--- src/components/Video/DaoTongPlayer/index.tsx
+++ src/components/Video/DaoTongPlayer/index.tsx
@@ -10,29 +10,55 @@ const DaoTongPlayer: React.FC<PropsType> = (props) => {
   const ref = useRef<HTMLVideoElement>(null)
   useEffect(() => {
     if (window.AutelMedia) {
-      const instance = new window.AutelMedia.Player()
-      // 设置连接前缀
-      instance.setProxyPrefix(
-        `${globalConfig.globalWs}://${location.host}/_ws_proxy/`,
-      )
-      // 开始播放
-      instance
-        .play({
-          // 设备SN
-          id: sn,
-          // 视频元素
-          videoElement: ref.current!,
-          // 拉流协议
-          protocol: 'rtc-rtc',
-          // 服务器地址
-          server: '/_proxy/' + globalConfig.daotongServer,
-        })
-        .then((res: number) => {
-          console.log('播放结果：', res === 0 ? '成功' : '失败')
-        })
+      // const instance = new window.AutelMedia.Player()
+      // // 设置连接前缀
+      // instance.setProxyPrefix(
+      //   `${globalConfig.globalWs}://${location.host}/_ws_proxy/`,
+      // )
+      // // 开始播放
+      // instance
+      //   .play({
+      //     // 设备SN
+      //     id: sn,
+      //     // 视频元素
+      //     videoElement: ref.current!,
+      //     // 拉流协议
+      //     protocol: 'rtc-rtc',
+      //     // 服务器地址
+      //     server: '/_proxy/' + globalConfig.daotongServer,
+      //   })
+      //   .then((res: number) => {
+      //     console.log('播放结果：', res === 0 ? '成功' : '失败')
+      //   })
 
+      const player = new window.AutelMedia(ref.current!, {
+        // 流id
+        streamId: `${sn}/live`,
+        // 服务器地址
+        serverUrl: '/_proxy/' + globalConfig.daotongServer,
+        // 播放失败的回调  errorcode 错误码  error 错误信息
+        onerror: (errorcode, error: any) => {},
+        // 播放开始的回调
+        onstart: () => {},
+        // 播放成功的回调
+        onsuccess: () => {},
+        // 播放信息回调 比特率/码率 bitrate  丢包数 lostrate
+        oninfo: ({
+          bitrate,
+          lostrate,
+        }: {
+          bitrate: string
+          lostrate: string
+        }) => {},
+        // 播放状态回调 status 链接状态 AConnectStatus    reason 原因
+        onstatus: (status, reason: string) => {},
+      })
+
+      // 开始播放
+      player.startPlay()
       return () => {
-        instance.stop()
+        // instance.stop()
+        player?.stopPlay()
       }
     }
   }, [sn])
---
+++ vite-config/dev/config.zhongshan.ts
@@ -0,0 +1,52 @@
+import { defineConfig, mergeConfig } from 'vite'
+import commonConfig from '../common'
+
+export default mergeConfig(
+  commonConfig,
+  defineConfig({
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        // 4A 接口
+        '/proxyApi': {
+          target: 'http:/10.20.23.62:32711/',
+          // target: 'http://127.0.0.1:7001/',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/proxyApi/, '/'),
+        },
+        // 全局 WebSocket
+        '/ws': {
+          target: 'http:/10.20.23.62:32201',
+          ws: true,
+          changeOrigin: true,
+        },
+        // 设备websocket直连
+        '/v3': {
+          target: 'https:/10.20.23.62:32713',
+          ws: true,
+          changeOrigin: true,
+        },
+        '/storage': {
+          target: 'http:/10.20.23.62:32205',
+          changeOrigin: true,
+        },
+        '/data': {
+          target: 'http:/10.20.23.62:32650',
+          changeOrigin: true,
+        },
+        // 瓦片地图
+        '/ja-map': {
+          target: 'http:/10.20.23.62:32712',
+          changeOrigin: true,
+        },
+      },
+    },
+    define: {
+      // 用于合并 window.config 的配置
+      __DEV_MERGE_CONFIG__: {
+        systemName: 'jingqi',
+        loginUrl: 'https:/10.20.23.62:32712/login',
+      },
+    },
+  }),
+)
