7fb171c088b1b18b1ae1d50f9cf51138aad3ed1b
CoderYoo <1084063695@qq.com>
2025-01-22 14:53:44 +0800

    feat: langs dict

--- src/service/modules/dict/index.ts
+++ src/service/modules/dict/index.ts
@@ -1,5 +1,12 @@
+import serverControlCenter from '@/service/servers/serverControlCenter'
 import serverJingqi from '@/service/servers/serverJingqi'
 
 export const getDictList = (data = {}) => {
   return serverJingqi.post<API_DICT.res.DictListRes>('/dict/list', data)
+}
+
+export const getControlCenterDictList = () => {
+  return serverControlCenter.post<API_DICT.res.ControlCenterDictList>(
+    '/v3/dji/dict/list',
+  )
 }
--- src/store/useDict.store.ts
+++ src/store/useDict.store.ts
@@ -1,14 +1,17 @@
+import { emtpyObject } from '@/constant/data'
 import { DictEnum } from '@/enum/dict'
-import { getDictList } from '@/service/modules/dict'
+import { getControlCenterDictList, getDictList } from '@/service/modules/dict'
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
 type StateType = {
   dict: Record<string, Record<string, API_DICT.domain.DictRecord>> | null
+  langsDict: Record<string, Record<string, Record<string, string | undefined>>>
 }
 
 type ActionsType = {
-  fetchDict: () => void
+  updateDict: (newDict: StateType['dict']) => void
+  updateLangsDict: (newDict: StateType['langsDict']) => void
 }
 
 /** 字典信息 */
@@ -16,16 +19,12 @@ const useDictStore = create<StateType & ActionsType>()(
   devtools(
     (set) => ({
       dict: null,
-      fetchDict: async () => {
-        const resp = await getDictList()
-        const newDict: StateType['dict'] = {}
-        for (const item of resp.data) {
-          newDict[item.dictGroup] = {
-            ...newDict[item.dictGroup],
-            [item.dictKey]: item,
-          }
-        }
-        set({ dict: newDict }, false, 'fetchDict')
+      langsDict: {},
+      updateDict: async (newDict) => {
+        set({ dict: newDict }, false, 'updateDict')
+      },
+      updateLangsDict: async (newDict) => {
+        set({ langsDict: newDict }, false, 'updateLangsDict')
       },
     }),
     {
@@ -36,16 +35,32 @@ const useDictStore = create<StateType & ActionsType>()(
 )
 
 export const useDictOptions = (dictGroup: DictEnum) => {
-  const dict = useDictStore((state) => state.dict)
+  const queryClient = useQueryClient()
+  const { data } = useQuery(
+    {
+      queryKey: ['dict'],
+      queryFn: () => getDictList(),
+      staleTime: Infinity,
+      select: (d) => d.data,
+    },
+    queryClient,
+  )
 
-  // 懒加载字典数据
   useEffect(() => {
-    if (useDictStore.getState().dict === null) {
-      useDictStore.getState().fetchDict()
-      useDictStore.getState().dict = {}
+    if (!data) {
+      return
     }
-  }, [dict])
+    const newDict: StateType['dict'] = {}
+    for (const item of data) {
+      newDict[item.dictGroup] = {
+        ...newDict[item.dictGroup],
+        [item.dictKey]: item,
+      }
+    }
+    useDictStore.getState().updateDict(newDict)
+  }, [data])
 
+  const dict = useDictStore((state) => state.dict)
   return useMemo(
     () =>
       Object.values(dict?.[dictGroup] || {})
@@ -55,6 +70,47 @@ export const useDictOptions = (dictGroup: DictEnum) => {
           value: item.dictKey,
         })),
     [dict, dictGroup],
+  )
+}
+
+/** 国际化相关的字典 */
+export const useLangsDict = (type: string) => {
+  const queryClient = useQueryClient()
+  const { i18n } = useTranslation()
+
+  const { data } = useQuery(
+    {
+      queryKey: ['langsDict'],
+      queryFn: () => getControlCenterDictList(),
+      staleTime: Infinity,
+      select: (d) => d.data,
+    },
+    queryClient,
+  )
+
+  useEffect(() => {
+    if (!data) {
+      return
+    }
+    const newDict: StateType['langsDict'] = {
+      zh: {},
+      en: {},
+    }
+    for (const item of data) {
+      if (!newDict['zh'][item.type]) {
+        newDict['zh'][item.type] = {}
+      }
+      if (!newDict['en'][item.type]) {
+        newDict['en'][item.type] = {}
+      }
+      newDict['zh'][item.type][item.code] = item.msgZhCn
+      newDict['en'][item.type][item.code] = item.msgEnUs
+    }
+    useDictStore.getState().updateLangsDict(newDict)
+  }, [data])
+
+  return useDictStore(
+    (state) => state.langsDict[i18n.language]?.[type] ?? emtpyObject,
   )
 }
 
