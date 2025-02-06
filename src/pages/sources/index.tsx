c81bf800d850276109f8933cf097a4e860fe2762
CoderYoo <1084063695@qq.com>
2025-02-06 17:36:29 +0800

    feat: Add device type weight sorting and improve source type selection

--- src/pages/sources/index.tsx
+++ src/pages/sources/index.tsx
@@ -4,6 +4,7 @@ import SourceTable from './components/SourceTable'
 import { useSearchParams } from 'react-router-dom'
 import { isNil } from 'lodash'
 import { useLangsDict } from '@/store/useDict.store'
+import { getDeviceWeight } from '@/enum/device'
 
 type PropsType = unknown
 
@@ -22,14 +23,25 @@ const PageSources: FC<PropsType> = memo(() => {
     queryClient,
   )
 
+  const dict = useLangsDict('device_type')
+  const renderItems = useMemo(() => {
+    if (!data) {
+      return []
+    }
+    return data
+      .map((e) => ({
+        label: dict[e.type] ?? e.name,
+        value: e.type,
+      }))
+      .sort((a, b) => getDeviceWeight(a.value) - getDeviceWeight(b.value))
+  }, [data, dict])
+
   // 如果不存在 type, 默认选中第一个
   useEffect(() => {
-    if (!searchParams.get('type') && data?.at(0)) {
-      setSearchParams({ type: data.at(0)!.type })
+    if (!searchParams.get('type') && renderItems[0]) {
+      setSearchParams({ type: renderItems[0].value })
     }
-  }, [data])
-
-  const dict = useLangsDict('device_type')
+  }, [renderItems])
 
   return (
     <div className="page-full p-3 bg-ground-2 flex flex-col overflow-y-hidden">
@@ -43,9 +55,9 @@ const PageSources: FC<PropsType> = memo(() => {
           <Tabs
             className="mt-3"
             activeKey={searchParams.get('type') ?? undefined}
-            items={data.map((e) => ({
-              key: e.type,
-              label: dict[e.type] ?? e.name,
+            items={renderItems.map((e) => ({
+              key: e.value,
+              label: e.label,
             }))}
             onChange={(e) => !isNil(e) && setSearchParams({ type: e })}
           />
