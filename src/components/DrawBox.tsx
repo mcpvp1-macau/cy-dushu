9bc87f492693470bede2585f5c2dfa752c47b02c
CoderYoo <1084063695@qq.com>
2025-02-19 15:31:59 +0800

    fix: prevent unnecessary execution when mouse is not drawing

--- src/components/DrawBox.tsx
+++ src/components/DrawBox.tsx
@@ -69,6 +69,9 @@ const DrawBox: FC<PropsType> = memo(({ onDrawEnd }) => {
   )
 
   const handleMouseUp = useMemoizedFn(() => {
+    if (!isDrawing.current) {
+      return
+    }
     isDrawing.current = false
     onDrawEnd?.(rect)
     setTimeout(() => {
