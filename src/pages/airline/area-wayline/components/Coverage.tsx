c3b45b0295a8c0aafe4b99ecc05a6386cc6dee52
CoderYoo <1084063695@qq.com>
2025-02-25 14:54:19 +0800

    feat: improve area wayline configuration with tooltips and store reset

--- src/pages/airline/area-wayline/components/Coverage.tsx
+++ src/pages/airline/area-wayline/components/Coverage.tsx
@@ -2,6 +2,9 @@ import XCard from '@/components/ui/XCard'
 import useAreaWaylineStore from '@/store/uav/uav-area-wayline/useAreaWayline.store'
 import HNumber from '../../edit/components/HNumber'
 import { calcFovRadiation } from '@/utils/fov'
+import { InfoCircleOutlined } from '@ant-design/icons'
+import { Tooltip } from 'antd'
+import coverageImg from '../assets/coverage.png'
 
 type PropsType = unknown
 
@@ -39,7 +42,24 @@ const Coverage: FC<PropsType> = memo(() => {
   }, [coverage, height])
 
   return (
-    <XCard title={t('wayline.waylineConfig.coverage.title')}>
+    <XCard
+      title={
+        <div className="flex items-center gap-1">
+          {t('wayline.waylineConfig.coverage.title')}
+          <Tooltip
+            className="ml-1"
+            title={
+              <div className="flex flex-col items-center gap-1">
+                <p>{t('wayline.waylineConfig.coverage.tooltip')}</p>
+                <img src={coverageImg} className="w-40" alt="coverage" />
+              </div>
+            }
+          >
+            <InfoCircleOutlined />
+          </Tooltip>
+        </div>
+      }
+    >
       <HNumber
         className="mt-3"
         negatives={[-10, -1]}
