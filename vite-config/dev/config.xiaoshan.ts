cef77865c50e047b0e798a1e465975a243064200
17758000300 <1056449783@qq.com>
2025-03-05 20:54:33 +0800

    fix: 视频框选坐标数组转字符串

--- vite-config/dev/config.xiaoshan.ts
+++ vite-config/dev/config.xiaoshan.ts
@@ -8,7 +8,7 @@ export default mergeConfig(
       proxy: {
         // 4A 接口
         '/proxyApi': {
-          target: 'http://172.31.0.19:31851',
+          target: 'http://172.31.0.19:32711',
           // target: 'http://127.0.0.1:7001/',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/proxyApi/, '/'),
