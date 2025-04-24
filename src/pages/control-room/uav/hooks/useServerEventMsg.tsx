14aa94929ea3f3c589713d32de45a9b3b3b60200
CoderYoo <1084063695@qq.com>
2025-04-24 11:11:48 +0800

    feat: add close button

--- src/pages/control-room/uav/hooks/useServerEventMsg.tsx
+++ src/pages/control-room/uav/hooks/useServerEventMsg.tsx
@@ -2,6 +2,8 @@ import { useAppMsg } from '@/hooks/useAppMsg'
 import { MessageInstance } from 'antd/es/message/interface'
 import { v4 as uuidv4 } from 'uuid'
 import { takePhotoEventEmitter } from '../components/ControlRoomVideo/hooks/useHandleTakePhotoEvent'
+import IconButton from '@/components/ui/button/IconButton'
+import IconClose from '@/assets/icons/jsx/IconClose'
 
 /** 处理 Websocket 服务端来的消息弹窗 */
 const useServerEventMsg = (msgApi?: MessageInstance) => {
@@ -22,13 +24,20 @@ const useServerEventMsg = (msgApi?: MessageInstance) => {
         msgApi.open({
           key: id,
           type,
-          content: wsData.data.message,
+          content: wsData.data.needConfirm ? (
+            <div className="flex items-center gap-1">
+              <p>{wsData.data.message}</p>
+              <IconButton
+                className="text-lg"
+                onClick={() => msgApi.destroy(id)}
+              >
+                <IconClose />
+              </IconButton>
+            </div>
+          ) : (
+            wsData.data.message
+          ),
           duration: wsData.data.needConfirm ? 0 : 3,
-          onClose: wsData.data.needConfirm
-            ? () => {
-                msgApi.destroy(id)
-              }
-            : undefined,
         })
       }
     }
