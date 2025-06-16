7cfeb5cbf7e5dcd36ccab33dc2e2c0e3d83f403f
CoderYoo <1084063695@qq.com>
2025-06-16 15:28:03 +0800

    feat: support changing action type

---
+++ src/pages/situation/action/detail/components/ActionTypeCheckout.tsx
@@ -0,0 +1,64 @@
+import IconButton from '@/components/ui/button/IconButton'
+import FormModal from '@/components/XForm/Modal'
+import { DictEnum } from '@/enum/dict'
+import { updAction } from '@/service/modules/action'
+import { useDictOptions } from '@/store/useDict.store'
+import { AppstoreOutlined } from '@ant-design/icons'
+
+type PropsType = {
+  data: API_ACTION.domain.ActionDetail
+}
+
+/** 行动类型切换 */
+const ActionTypeCheckout: FC<PropsType> = memo(({ data }) => {
+  const [open, { setTrue, setFalse }] = useBoolean()
+
+  const actionTypeOptions = useDictOptions(DictEnum.ACTION_TYPE)
+
+  const queryClient = useQueryClient()
+  const handleConfirm = async (values) => {
+    await updAction({
+      id: data.id,
+      name: data.name,
+      type: values.actionType,
+    })
+    setFalse()
+    await queryClient.invalidateQueries({
+      queryKey: ['action', data.id + ''],
+    })
+  }
+
+  return (
+    <>
+      <IconButton
+        className="scale-125 mr-2"
+        toolTipProps={{ title: '修改行动类型' }}
+        onClick={setTrue}
+      >
+        <AppstoreOutlined />
+      </IconButton>
+      <FormModal
+        key={data?.type}
+        open={open}
+        title="修改行动类型"
+        initialValues={{
+          actionType: data?.type,
+        }}
+        items={[
+          {
+            label: '行动类型',
+            name: 'actionType',
+            type: 'select',
+            options: actionTypeOptions,
+          },
+        ]}
+        onConfirm={handleConfirm}
+        onClose={setFalse}
+      />
+    </>
+  )
+})
+
+ActionTypeCheckout.displayName = 'ActionTypeCheckout'
+
+export default ActionTypeCheckout
--- src/pages/situation/action/detail/components/kcyp/shanghai/Panel.tsx
+++ src/pages/situation/action/detail/components/kcyp/shanghai/Panel.tsx
@@ -9,6 +9,7 @@ import { Form } from 'antd'
 import { DictEnum } from '@/enum/dict'
 import useWatch from '@/hooks/useWatch'
 import dayjs from 'dayjs'
+// import '../kcform.less'
 
 type PropsType = { actionId: string }
 
@@ -119,20 +120,23 @@ const KCYPNormalPanel: FC<PropsType> = memo(({ actionId }) => {
           {statusColorMap[data.processStatus!]?.label[i18n.language] || '-'}
         </span>
       </p>
-      <XForm
-        form={form}
-        disabled={data.processStatus !== ProcessStatusEnum.INIT}
-        items={formItems}
-        layout="vertical"
-        colsProps={{ span: 12 }}
-        rowsProps={{ gutter: 12 }}
-        themeConfig={{
-          verticalLabelPadding: 4,
-          labelFontSize: 12,
-          itemMarginBottom: 0,
-        }}
-        onValuesChange={handleValuesChange}
-      />
+      <div className="kcform">
+        <XForm
+          form={form}
+          disabled={data.processStatus !== ProcessStatusEnum.INIT}
+          items={formItems}
+          layout="vertical"
+          colsProps={{ span: 12 }}
+          rowsProps={{ gutter: 12 }}
+          themeKey="dushu-kcyp"
+          themeConfig={{
+            verticalLabelPadding: 4,
+            labelFontSize: 12,
+            itemMarginBottom: 0,
+          }}
+          onValuesChange={handleValuesChange}
+        />
+      </div>
     </div>
   )
 })
