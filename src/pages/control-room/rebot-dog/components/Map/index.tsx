f3bd797ccba1c0360cc3e56738279795874d567a
CoderYoo <1084063695@qq.com>
2025-06-17 14:07:25 +0800

    feat: add TargetPoints to RebotDogMap

--- src/pages/control-room/rebot-dog/components/Map/index.tsx
+++ src/pages/control-room/rebot-dog/components/Map/index.tsx
@@ -1,11 +1,13 @@
 import CesiumMap from '@/map/CesiumMap'
 import RebotDogRealMarker from './components/RealMarker'
 import RebotDogRealTrack from './components/RealTrack'
+import TargetPoints from '@/map/GlobalMap/TargetPoints'
 const RebotDogMap: FC<unknown> = memo(() => {
   return (
     <CesiumMap id="rebot-dog-control-room-map">
       <RebotDogRealMarker />
       <RebotDogRealTrack />
+      <TargetPoints />
     </CesiumMap>
   )
 })
