23f4b4c74b4bc92f5e3de1820066278a543bbd25
CoderYoo <1084063695@qq.com>
2025-09-09 12:00:31 +0800

    feat(uav): 根据链路和有无机巢决定起飞方式
    调整“一键起飞”的指令下发逻辑：
    
    1. 如果当前链路是5G，或无人机没有父设备（不在机巢中），则直接从无人机本身下发起飞指令。
    
    2. 否则（在机巢中且非5G链路），则从父设备（机巢）下发起飞指令。
    
    此项改动使起飞功能可以适配更多应用场景。同时，也统一了弹窗中输入框的样式。

--- src/pages/control-room/uav/components/AsideButtons/Takeoff.tsx
+++ src/pages/control-room/uav/components/AsideButtons/Takeoff.tsx
@@ -4,6 +4,8 @@ import FormModal from '@/components/XForm/Modal'
 import { useDeviceDetailStore } from '@/pages/right/DeviceDetail/hooks/useDeviceDetail.store'
 import { useUavControlRoomStore } from '@/store/context-store/useUavControlRoom.store'
 import useIdleControlTag from '../../hooks/useIdleControlTag'
+import { usePostDeviceServiceHandler } from '@/hooks/device/usePostDeviceService'
+import { getDeviceDetail } from '@/service/modules/device'
 
 type PropsType = {
   postServiceFn: (identifier: string, data?: any) => Promise<void>
@@ -17,11 +19,27 @@ const Takeoff: FC<PropsType> = memo(({ postServiceFn }) => {
   const idleControlTag = useIdleControlTag()
   const hasService = useDeviceDetailStore((s) => s.serviceHave['takeoff'])
 
+  const parentId = useDeviceDetailStore((s) => s.deviceDetail?.parentId)
+
+  const links = useUavControlRoomStore((s) => s.links)
+  const currentLink = useMemo(
+    () => links?.find((link) => link.active)?.name ?? 'auto',
+    [links],
+  )
+
+  const postServicehandler = usePostDeviceServiceHandler()
+
   const canTakeoff =
     !isLimitedFly && (idleControlTag || hasControlPower) && hasService
 
   const handleClick = async (data) => {
-    await postServiceFn('takeoff', data)
+    if (!parentId || currentLink?.toUpperCase?.() === '5G') {
+      await postServiceFn('takeoff', data)
+    } else {
+      const resp = await getDeviceDetail(parentId)
+      const productKey = resp.data.deviceModel.productKey
+      await postServicehandler(productKey, parentId, 'takeoff', data)
+    }
     setFalse()
   }
 
@@ -48,7 +66,7 @@ const Takeoff: FC<PropsType> = memo(({ postServiceFn }) => {
               type: 'input-number',
               rules: [{ required: true, message: '请输入起飞高度' }],
               otherProps: {
-                addonAfter: 'm',
+                addonAfter: <div className="mx-1">m</div>,
                 min: 1,
                 max: globalConfig.uavHeightLimit,
               },
@@ -58,7 +76,7 @@ const Takeoff: FC<PropsType> = memo(({ postServiceFn }) => {
               name: 'gohomeAltitude',
               type: 'input-number',
               otherProps: {
-                addonAfter: 'm',
+                addonAfter: <div className="mx-1">m</div>,
                 min: 50,
                 max: globalConfig.uavHeightLimit,
               },
