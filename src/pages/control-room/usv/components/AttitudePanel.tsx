ca822539c2f9d76b6b0d9f2d75c0a40bd5cd720c
CoderYoo <1084063695@qq.com>
2025-12-10 19:55:34 +0800

    feat: ui adjust

--- src/pages/control-room/usv/components/AttitudePanel.tsx
+++ src/pages/control-room/usv/components/AttitudePanel.tsx
@@ -1,7 +1,7 @@
 import compassArrowImg from '@/assets/imgs/control/compass-arrow.svg'
 import compassRingImg from '@/assets/imgs/control/compass-ring.png'
+import LiqunTippy from '@/components/ui/LiqunTippy'
 import { useUsvControlRoomStore } from '@/store/context-store/useUsvControlRoom.store'
-import { Tooltip } from 'antd'
 
 const AttitudePanel: FC = memo(() => {
   const heading = useUsvControlRoomStore((s) => s.state.heading) ?? 0
@@ -11,7 +11,7 @@ const AttitudePanel: FC = memo(() => {
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="relative h-32 w-32 pointer-events-none">
+      <div className="relative h-32 w-32">
         <div className="absolute inset-5 overflow-hidden rounded-full">
           <div
             className="absolute inset-0 -left-80 -right-80 bg-gradient-to-b from-[#4c90f055] to-transparent"
@@ -31,33 +31,36 @@ const AttitudePanel: FC = memo(() => {
           <img src={compassArrowImg} className="scale-90" />
         </div>
 
-        <Tooltip
-          className="pointer-events-auto"
-          title={t('usv.attitude.heading', '艏向')}
-        >
+        <LiqunTippy content={t('usv.attitude.heading', '艏向')}>
           <div
             className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-full whitespace-nowrap text-green-500"
             style={{ textShadow: '0 0 2px #000' }}
           >
             ψ {heading.toFixed(1)}°
           </div>
-        </Tooltip>
-        <Tooltip className="pointer-events-auto" title={t('usv.attitude.pitch', '纵摇')}>
+        </LiqunTippy>
+        <LiqunTippy
+          className="pointer-events-auto"
+          content={t('usv.attitude.pitch', '纵摇')}
+        >
           <div
-            className="absolute bottom-0 left-0 translate-y-full whitespace-nowrap text-green-500"
+            className="absolute bottom-0 left-0 translate-y-full whitespace-nowrap text-green-500 select-none"
             style={{ textShadow: '0 0 2px #000' }}
           >
             θ {pitch.toFixed(1)}°
           </div>
-        </Tooltip>
-        <Tooltip className="pointer-events-auto" title={t('usv.attitude.roll', '横摇')}>
+        </LiqunTippy>
+        <LiqunTippy
+          className="pointer-events-auto"
+          content={t('usv.attitude.roll', '横摇')}
+        >
           <div
-            className="absolute bottom-0 right-0 translate-y-full whitespace-nowrap text-green-500"
+            className="absolute bottom-0 right-0 translate-y-full whitespace-nowrap text-green-500 select-none"
             style={{ textShadow: '0 0 2px #000' }}
           >
             ϕ {roll.toFixed(1)}°
           </div>
-        </Tooltip>
+        </LiqunTippy>
       </div>
     </div>
   )
