9ec0a58c196cb7fef4ac60726e78df7c3d64da60
祐树 <65854993+coderyoo1@users.noreply.github.com>
2025-12-10 19:47:19 +0800

    Add i18n tooltips for USV attitude

--- src/pages/control-room/usv/components/AttitudePanel.tsx
+++ src/pages/control-room/usv/components/AttitudePanel.tsx
@@ -7,6 +7,7 @@ const AttitudePanel: FC = memo(() => {
   const heading = useUsvControlRoomStore((s) => s.state.heading) ?? 0
   const pitch = useUsvControlRoomStore((s) => s.state.pitch) ?? 0
   const roll = useUsvControlRoomStore((s) => s.state.roll) ?? 0
+  const { t } = useTranslation()
 
   return (
     <div className="flex h-full w-full items-center justify-center">
@@ -30,7 +31,10 @@ const AttitudePanel: FC = memo(() => {
           <img src={compassArrowImg} className="scale-90" />
         </div>
 
-        <Tooltip className="pointer-events-auto" title="艏向">
+        <Tooltip
+          className="pointer-events-auto"
+          title={t('usv.attitude.heading', '艏向')}
+        >
           <div
             className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-full whitespace-nowrap text-green-500"
             style={{ textShadow: '0 0 2px #000' }}
@@ -38,7 +42,7 @@ const AttitudePanel: FC = memo(() => {
             ψ {heading.toFixed(1)}°
           </div>
         </Tooltip>
-        <Tooltip className="pointer-events-auto" title="纵摇">
+        <Tooltip className="pointer-events-auto" title={t('usv.attitude.pitch', '纵摇')}>
           <div
             className="absolute bottom-0 left-0 translate-y-full whitespace-nowrap text-green-500"
             style={{ textShadow: '0 0 2px #000' }}
@@ -46,7 +50,7 @@ const AttitudePanel: FC = memo(() => {
             θ {pitch.toFixed(1)}°
           </div>
         </Tooltip>
-        <Tooltip className="pointer-events-auto" title="横摇">
+        <Tooltip className="pointer-events-auto" title={t('usv.attitude.roll', '横摇')}>
           <div
             className="absolute bottom-0 right-0 translate-y-full whitespace-nowrap text-green-500"
             style={{ textShadow: '0 0 2px #000' }}
