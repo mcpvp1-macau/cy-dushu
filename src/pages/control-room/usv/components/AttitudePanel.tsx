91d4d6f49680c96f624a868f2be236964f8b02f5
祐树 <65854993+coderyoo1@users.noreply.github.com>
2025-12-10 18:54:03 +0800

    Enhance USV attitude panel visualization

--- src/pages/control-room/usv/components/AttitudePanel.tsx
+++ src/pages/control-room/usv/components/AttitudePanel.tsx
@@ -1,16 +1,53 @@
-import { Statistic } from 'antd'
+import compassArrowImg from '@/assets/imgs/control/compass-arrow.svg'
+import compassRingImg from '@/assets/imgs/control/compass-ring.png'
 import { useUsvControlRoomStore } from '@/store/context-store/useUsvControlRoom.store'
 
 const AttitudePanel: FC = memo(() => {
-  const heading = useUsvControlRoomStore((s) => s.state.heading ?? 0)
-  const pitch = useUsvControlRoomStore((s) => s.state.pitch ?? 0)
-  const roll = useUsvControlRoomStore((s) => s.state.roll ?? 0)
+  const heading = useUsvControlRoomStore((s) => s.state.heading) ?? 0
+  const pitch = useUsvControlRoomStore((s) => s.state.pitch) ?? 0
+  const roll = useUsvControlRoomStore((s) => s.state.roll) ?? 0
 
   return (
-    <div className="grid size-full grid-cols-3 gap-4 p-4">
-      <Statistic title="Heading" value={heading} precision={2} suffix="°" />
-      <Statistic title="Pitch" value={pitch} precision={2} suffix="°" />
-      <Statistic title="Roll" value={roll} precision={2} suffix="°" />
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="relative h-40 w-40 pointer-events-none">
+        <div className="absolute inset-5 overflow-hidden rounded-full">
+          <div
+            className="absolute inset-0 -left-80 -right-80 bg-gradient-to-b from-[#4c90f055] to-transparent"
+            style={{
+              top: `${((pitch + 90) / 180) * 100}%`,
+              transformOrigin: 'top center',
+              rotate: `${roll}deg`,
+            }}
+          />
+        </div>
+
+        <div className="absolute inset-0" style={{ rotate: `${-heading}deg` }}>
+          <img src={compassRingImg} className="absolute inset-0 top-[-1px]" />
+        </div>
+
+        <div className="abs-center">
+          <img src={compassArrowImg} className="scale-90" />
+        </div>
+
+        <div
+          className="absolute left-1/2 top-0 -translate-x-1/2 -translate-y-full whitespace-nowrap text-green-500"
+          style={{ textShadow: '0 0 2px #000' }}
+        >
+          艏向 ψ {heading.toFixed(1)}°
+        </div>
+        <div
+          className="absolute bottom-0 left-0 translate-y-full whitespace-nowrap text-green-500"
+          style={{ textShadow: '0 0 2px #000' }}
+        >
+          纵摇 θ {pitch.toFixed(1)}°
+        </div>
+        <div
+          className="absolute bottom-0 right-0 translate-y-full whitespace-nowrap text-green-500"
+          style={{ textShadow: '0 0 2px #000' }}
+        >
+          横摇 ϕ {roll.toFixed(1)}°
+        </div>
+      </div>
     </div>
   )
 })
