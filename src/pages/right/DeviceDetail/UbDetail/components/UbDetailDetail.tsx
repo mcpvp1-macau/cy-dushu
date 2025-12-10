871569f5f9ac523ef896016b17ec84dd4c15b940
祐树 <65854993+coderyoo1@users.noreply.github.com>
2025-12-10 15:46:59 +0800

    Add live video controls to unmanned boat detail

--- src/pages/right/DeviceDetail/UbDetail/components/UbDetailDetail.tsx
+++ src/pages/right/DeviceDetail/UbDetail/components/UbDetailDetail.tsx
@@ -1,15 +1,22 @@
+import DeviceLiveVideo from '@/components/VideoS/DeviceLiveVideo'
 import UbInfoCard from './UbInfoCard'
 import { useDeviceDetailStore } from '../../hooks/useDeviceDetail.store'
 import { useRealOnlineStatus } from '@/store/useGlobalWebSocket.store'
+import VideoSnapshotBtn from '@/hooks/device/VideoSnapshot'
+import { ComponentRef } from 'react'
 
 const UbDetailDetail: FC = memo(() => {
   const deviceDetail = useDeviceDetailStore((s) => s.deviceDetail)
   const deviceId = useDeviceDetailStore((s) => s.deviceId)
+  const productKey = useDeviceDetailStore((s) => s.productKey)
 
   const modelNumber =
     deviceDetail?.deviceTags?.find(
       (item: { tagName: string }) => item.tagName === 'MODEL_NUMBER',
     )?.tagValue || '-'
+
+  const videoRef = useRef<ComponentRef<typeof DeviceLiveVideo>>(null)
+  const videoId = deviceDetail?.properties.videoList?.[0]?.videoId ?? ''
 
   const properties = deviceDetail?.properties ?? {}
 
@@ -32,6 +39,21 @@ const UbDetailDetail: FC = memo(() => {
         heading={heading}
         speed={speed}
       />
+      <div className="m-3">
+        <DeviceLiveVideo
+          ref={videoRef}
+          productKey={productKey}
+          deviceId={deviceId}
+          videoId={videoId}
+          rightTop={
+            <VideoSnapshotBtn
+              productKey={productKey}
+              deviceId={deviceId}
+              videoLiveRef={videoRef}
+            />
+          }
+        />
+      </div>
     </div>
   )
 })
