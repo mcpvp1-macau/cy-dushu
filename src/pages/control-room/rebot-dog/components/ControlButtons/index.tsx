39c582a9d3a8253c1c711c889b1e34d15ee66923
祐树 <65854993+coderyoo1@users.noreply.github.com>
2025-12-16 15:53:37 +0800

    Include translation in robot dog control memo deps

--- src/pages/control-room/rebot-dog/components/ControlButtons/index.tsx
+++ src/pages/control-room/rebot-dog/components/ControlButtons/index.tsx
@@ -150,7 +150,7 @@ const RebotDogControlButtons: FC = memo(() => {
           }),
         },
       ] as Btn[],
-    [params],
+    [params, t],
   )
 
   const dogControlInfo = useRebotDogControlRoomStore((s) => s.dogControlInfo)
